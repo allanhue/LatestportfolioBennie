9ad4ef1b8ff2fe4eb0bb1a647433aeebeaf48c08
allanhue <allanmwangi329@gmail.com>
2025-07-08 16:08:33 +0300

    issue updated in index.html

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
            <a href="https://github.com/allanhue/LatestportfolioBennie">View on GitHub</a>
 
         </div>
-      
+      <br />
        
         <div>
           <h2>Pet Projects</h2>
@@ -21,6 +21,7 @@ function Projects() {
           {/* link redirect to another window */}
           <a href="https://rianac.netlify.app/" target="_blank" rel="noopener noreferrer">View application</a>
         </div>
+        <br />
         <div>
           <h2>Data Analysis Projects</h2>
           <p>Done some data analysis  and cleaning  datasets projects which build my programming experience with Python when I started early my career . This gave me a chance to know where I stood  on understanding key concepts. One was a sales project, another was a money laundering project. All resources i used kaggle for learning and implementation</p>
