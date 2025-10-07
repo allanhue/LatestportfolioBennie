de8b23bc768e9b339fcf4dee88b068cb55cbcacd
allanhue <allanmwangi329@gmail.com>
2025-10-07 16:24:40 +0300

    added sofware enginnerring

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,7 +29,7 @@ function About () {
             {/* <strong>Software Engineer</strong>  */}
             {/* and{' '} */}
             <strong>Data Science Practitioner</strong> 
-            with a passion for building
+               with a passion for building
             data-driven solutions. Certified by <strong>IBM (Coursera)</strong>{' '}
             and <strong>Kaggle</strong>, I specialize in{' '}
             <strong>
